c21b5502b901c81ecfb7ed16e7eaabfa3b913959
kangyoolee <e.kangyoo@gmail.com>
2022-10-22 10:15:34 +0900

    dlaha

---
+++ review.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+
+url = 'https://entertain.naver.com/ranking/comment/list?oid=022&aid=0003409199'
+
+#웹 드라이버
+driver = webdriver.Chrome()
+driver.get('https://blog.naver.com/go4693')x
